09c842d18e63fec3e8ab642ec568deb65e514ecd
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-01-14 16:20:41 +0300

    Headless mode for window

--- first_task.py
+++ first_task.py
@@ -5,6 +5,9 @@ import logging
 import dateparser
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 from datetime import datetime, timedelta
 
 
@@ -65,94 +68,103 @@ def parse_reddit_page():
     else:
         logger.error("The file was not created!")
 
-    browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-    browser.maximize_window()
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+
+    browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
     browser.get("https://www.reddit.com/top/?t=month")
     body_tag, total_posts_count, parsed_post_count = browser.find_element_by_tag_name("body"), 0, 0
+    try:
+        while parsed_post_count < 100:
+            html = browser.page_source
+            single_posts = get_posts_list(html)
+            post = single_posts[total_posts_count]
+            post_id = post["id"]
 
-    while parsed_post_count < 100:
-        html = browser.page_source
-        single_posts = get_posts_list(html)
-        post = single_posts[total_posts_count]
-        post_id = post["id"]
+            current_post = browser.find_element_by_id(post_id)
+            hover = webdriver.ActionChains(browser).move_to_element(current_post)
+            hover.perform()
 
-        current_post = browser.find_element_by_id(post_id)
-        hover = webdriver.ActionChains(browser).move_to_element(current_post)
-        hover.perform()
+            votes_number = post.select_one("div > div > div").get_text()
+            post_url = post.find("a", class_="_3jOxDPIQ0KaOWpzvSQo-1s")["href"]
+            post_category_wrapper = post.find("div", class_="_3AStxql1mQsrZuUIFP9xSg nU4Je7n-eSXStTBAPMYt8")
 
-        votes_number = post.select_one("div > div > div").get_text()
-        post_url = post.find("a", class_="_3jOxDPIQ0KaOWpzvSQo-1s")["href"]
-        post_category_wrapper = post.find("div", class_="_3AStxql1mQsrZuUIFP9xSg nU4Je7n-eSXStTBAPMYt8")
+            # Template: r/category_name
+            post_category = post_category_wrapper.find("a", class_="_3ryJoIoycVkA88fy40qNJc").get_text()
 
-        # Template: r/category_name
-        post_category = post_category_wrapper.find("a", class_="_3ryJoIoycVkA88fy40qNJc").get_text()
+            # Template: u/username
+            name_parse_string = post.find("a", class_="_2tbHP6ZydRpjI44J3syuqC _23wugcdiaj44hdfugIAlnX"
+                                                      " oQctV4n0yUb0uiHDdGnmE")
 
-        # Template: u/username
-        name_parse_string = post.find("a", class_="_2tbHP6ZydRpjI44J3syuqC _23wugcdiaj44hdfugIAlnX"
-                                                  " oQctV4n0yUb0uiHDdGnmE")
+            try:
+                username = name_parse_string.get_text()
+                user_page_url = "".join(["https://www.reddit.com", name_parse_string["href"]])
+            except AttributeError:
+                total_posts_count += 1
+                logger.debug(f"The post (post_id: {post_id}, url: {post_url}) exists, but the user has been deleted!")
+                continue
 
-        try:
-            username = name_parse_string.get_text()
-            user_page_url = "".join(["https://www.reddit.com", name_parse_string["href"]])
-        except AttributeError:
-            total_posts_count += 1
-            logger.debug(f"The post (post_id: {post_id}) exists, but the user has been deleted!")
-            continue
+            publish_date = post.find("a", class_="_3jOxDPIQ0KaOWpzvSQo-1s").get_text()
+            days_ago = int(publish_date.split(" ")[0])
+            post_date = datetime.today() - timedelta(days=days_ago)
 
-        publish_date = post.find("a", class_="_3jOxDPIQ0KaOWpzvSQo-1s").get_text()
-        days_ago = int(publish_date.split(" ")[0])
-        post_date = datetime.today() - timedelta(days=days_ago)
+            comments_number = post.select_one("div > div > div:nth-of-type(2)").find_all(recursive=False)[-1]
+            comments_number = comments_number.select("a > span")
 
-        comments_number = post.select_one("div > div > div:nth-of-type(2)").find_all(recursive=False)[-1]
-        comments_number = comments_number.select("a > span")
+            # Representation may have distinct html formats
+            if len(comments_number) == 1:
+                comments_number = comments_number[0].get_text().split(" ")[0]
+            else:
+                comments_number = comments_number[0].select_one("div").find_all(recursive=False)[-1].get_text()
 
-        # Representation may have distinct html formats
-        if len(comments_number) == 1:
-            comments_number = comments_number[0].get_text().split(" ")[0]
+            browser.execute_script(f"window.open('{user_page_url}');")
+            browser.switch_to.window(browser.window_handles[1])
+            user_page_html = browser.page_source
+            soup = BeautifulSoup(user_page_html, "html.parser")
+            user_profile_info_div_tag = soup.select_one("html > body > div:nth-of-type(1) > div > div:nth-of-type(2) >"
+                                                        "div:nth-of-type(2) > div > div > div > div:nth-of-type(2) >"
+                                                        "div:nth-of-type(4) > div:nth-of-type(2) > div >"
+                                                        "div:nth-of-type(1) > div > div:nth-of-type(4)")
+
+            browser.close()
+            browser.switch_to.window(browser.window_handles[0])
+            popup_menu = browser.find_element_by_id(f"UserInfoTooltip--{post_id}")
+            popup_menu = popup_menu.find_element_by_xpath("..")
+            hover = webdriver.ActionChains(browser).move_to_element(popup_menu)
+            hover.perform()
+
+            wait = WebDriverWait(browser, 20)
+            element = wait.until(
+                expected_conditions.element_to_be_clickable((By.ID, f"UserInfoTooltip--{post_id}-hover-id"))
+            )
+
+            popup_menu = BeautifulSoup(browser.page_source, "html.parser")\
+                .find("div", id=f"UserInfoTooltip--{post_id}-hover-id")\
+                .find_all("div", class_="_18aX_pAQub_mu1suz4-i8j")
+
+            post_karma, comment_karma = popup_menu[0].get_text(), popup_menu[1].get_text()
+
+            try:
+                total_posts_count += 1
+                user_karma = user_profile_info_div_tag.select_one("div > div > span").get_text()
+                user_cake_day = dateparser.parse(user_profile_info_div_tag
+                                                 .select_one("div:nth-of-type(2) > div > span")
+                                                 .get_text())
+            except AttributeError:
+                logger.debug(f"Failed to access user(username: {username}, link: {user_page_url}) page due to age limit!")
+                continue
+            else:
+                ParsedPostInfo(post_url, username.lstrip("u/"), user_karma, user_cake_day, post_karma,
+                               comment_karma, comments_number, votes_number, post_category.lstrip("r/"),
+                               post_date).write_to_file(filename)
+                logger.debug(f"All information has been received on this post(url: {post_url})")
+                parsed_post_count += 1
         else:
-            comments_number = comments_number[0].select_one("div").find_all(recursive=False)[-1].get_text()
-
-        browser.execute_script(f"window.open('{user_page_url}');")
-        browser.switch_to.window(browser.window_handles[1])
-        user_page_html = browser.page_source
-        soup = BeautifulSoup(user_page_html, "html.parser")
-        user_profile_info_div_tag = soup.select_one("html > body > div:nth-of-type(1) > div > div:nth-of-type(2) >"
-                                                    "div:nth-of-type(2) > div > div > div > div:nth-of-type(2) >"
-                                                    "div:nth-of-type(4) > div:nth-of-type(2) > div >"
-                                                    "div:nth-of-type(1) > div > div:nth-of-type(4)")
-
-        browser.close()
-        browser.switch_to.window(browser.window_handles[0])
-        popup_menu = browser.find_element_by_id(f"UserInfoTooltip--{post_id}")
-        popup_menu = popup_menu.find_element_by_xpath("..")
-        hover = webdriver.ActionChains(browser).move_to_element(popup_menu)
-        hover.perform()
-        time.sleep(2)
-
-        popup_menu = BeautifulSoup(browser.page_source, "html.parser")\
-            .find("div", id=f"UserInfoTooltip--{post_id}-hover-id")
-        popup_menu = popup_menu.find_all("div", class_="_18aX_pAQub_mu1suz4-i8j")
-        post_karma, comment_karma = popup_menu[0].get_text(), popup_menu[1].get_text()
-
-        try:
-            total_posts_count += 1
-            user_karma = user_profile_info_div_tag.select_one("div > div > span").get_text()
-            user_cake_day = dateparser.parse(user_profile_info_div_tag
-                                             .select_one("div:nth-of-type(2) > div > span")
-                                             .get_text())
-        except AttributeError:
-            logger.debug(f"Failed to access user(username: {username}, link: {user_page_url}) page due to age limit!")
-            continue
-        else:
-            ParsedPostInfo(post_url, username.lstrip("u/"), user_karma, user_cake_day, post_karma,
-                           comment_karma, comments_number, votes_number, post_category.lstrip("r/"),
-                           post_date).write_to_file(filename)
-            logger.debug(f"All information has been received on this post(url: {post_url})")
-            parsed_post_count += 1
-    else:
-        logger.info("100 records were successfully placed in the file!")
-
-    browser.quit()
+            logger.info("100 records were successfully placed in the file!")
+    except Exception as error:
+        print(error)
+        browser.quit()
 
 
 if __name__ == "__main__":
