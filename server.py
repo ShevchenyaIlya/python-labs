a9f1a989b42ff4d243550ce448a7e05ee0b99de2
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-01-20 15:50:56 +0300

    Extract functions that work with file from HTTPHandler. Fix mistakes in if statements. Develop command line arguments.

--- server.py
+++ server.py
@@ -8,18 +8,23 @@ from datetime import datetime
 
 class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
-        logging.info(f"GET request, Path: {self.path}")
-
         path_components = parse_url_path(self.path)
         path_components_len = len(path_components)
-        if path_components[0] == "posts" and path_components_len <= 2:
+        logging.info(f"GET request, Path: {self.path}")
+
+        if 0 < path_components_len <= 2 and path_components[0] == "posts":
             filename = generate_filename()
             if path_components_len == 2:
                 unique_id = path_components[1]
-                if self._get_single_post(filename, unique_id):
+                post = get_single_post(filename, unique_id)
+                if post is not None:
+                    self._set_response(200, "OK")
+                    self.wfile.write(json.dumps(deserialize_post_data(post)).encode("utf-8"))
                     return
             else:
-                self._get_all_posts(filename)
+                file_content = get_all_posts(filename)
+                self._set_response(200, "OK")
+                self.wfile.write(json.dumps(file_content).encode("utf-8"))
                 return
 
         self._set_response(404, "Not Found")
@@ -29,10 +34,11 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
         post_data = self._get_request_body()
         logging.info(f"POST request, Path: {str(self.path)}, Body: {post_data}")
 
-        if path_components[0] == "posts" and len(path_components) == 1:
+        if len(path_components) == 1 and path_components[0] == "posts":
             filename = generate_filename()
             if not file_exist(filename):
                 create_file(filename)
+                logging.info(f"File created(name: {filename})")
 
             unique_id = post_data["unique_id"]
             if not post_exist_in_file(filename, unique_id):
@@ -71,26 +77,6 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
         self._set_response(404, "Not Found")
 
-    def _get_single_post(self, filename: str, unique_id: str) -> bool:
-        if post_exist_in_file(filename, unique_id):
-            with open(filename, "r") as file:
-                for post in file:
-                    if unique_id in post:
-                        self._set_response(200, "OK")
-                        self.wfile.write(json.dumps(deserialize_post_data(post)).encode("utf-8"))
-                        return True
-
-        return False
-
-    def _get_all_posts(self, filename: str) -> None:
-        file_content = []
-        with open(filename, "r") as file:
-            for line in file:
-                file_content.append(deserialize_post_data(line))
-
-        self._set_response(200, "OK")
-        self.wfile.write(json.dumps(file_content).encode("utf-8"))
-
     def _get_request_body(self) -> dict:
         content_length = int(self.headers['Content-Length'])
         return json.loads(self.rfile.read(content_length).decode("utf-8"))
@@ -102,6 +88,25 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
     def _set_content_type(self) -> None:
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
+
+
+def get_single_post(filename: str, unique_id: str) -> str or None:
+    if post_exist_in_file(filename, unique_id):
+        with open(filename, "r") as file:
+            for post in file:
+                if unique_id in post:
+                    return post
+
+    return None
+
+
+def get_all_posts(filename: str) -> list:
+    file_content = []
+    with open(filename, "r") as file:
+        for line in file:
+            file_content.append(deserialize_post_data(line))
+
+    return file_content
 
 
 def delete_post(filename: str, unique_id: str) -> bool:
@@ -209,21 +214,33 @@ def run_server(port, server_class=ThreadingHTTPServer, handler_class=CustomHTTPR
     httpd = server_class(server_address, handler_class)
     try:
         httpd.serve_forever()
+        logging.info(f"Start server on port {port}")
     except KeyboardInterrupt as exception:
         logging.error(exception)
     finally:
         httpd.server_close()
+        logging.info(f"Server closed on port {port}")
 
 
-def parse_command_line_arguments() -> int:
+def string_to_logging_level(log_level: str) -> int:
+    possible_levels = {'DEBUG': logging.DEBUG, 'INFO': logging.INFO, 'WARNING': logging.WARNING,
+                       'ERROR': logging.ERROR, 'CRITICAL': logging.CRITICAL}
+
+    return possible_levels[log_level]
+
+
+def parse_command_line_arguments() -> tuple:
     argument_parser = argparse.ArgumentParser(description="Simple http server")
     argument_parser.add_argument("--port", metavar="port", type=int, default=8087)
+    argument_parser.add_argument("--log_level", metavar="log_level", type=str, default="CRITICAL",
+                                 choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                                 help="Minimal logging level('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')")
     args = argument_parser.parse_args()
 
-    return args.port
+    return args.port, args.log_level
 
 
 if __name__ == '__main__':
-    port_number = parse_command_line_arguments()
-    logging.basicConfig(level=logging.INFO)
+    port_number, logging_level = parse_command_line_arguments()
+    logging.basicConfig(level=string_to_logging_level(logging_level))
     run_server(port_number)
