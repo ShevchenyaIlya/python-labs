31d1e351ffa16bc02d32d87e5000a8213874f588
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-01-21 17:07:17 +0300

    String to logging level converter

---
+++ logging_converter.py
@@ -0,0 +1,8 @@
+import logging
+
+
+def string_to_logging_level(log_level: str) -> int:
+    possible_levels = {'DEBUG': logging.DEBUG, 'INFO': logging.INFO, 'WARNING': logging.WARNING,
+                       'ERROR': logging.ERROR, 'CRITICAL': logging.CRITICAL}
+
+    return possible_levels[log_level]
