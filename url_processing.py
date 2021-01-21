ee69d4e5b88cb0fad49d05cede95d39dfa109c11
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-01-21 18:53:50 +0300

    Rebuild endpoints representation than remove create_url function that never used.

--- url_processing.py
+++ url_processing.py
@@ -1,14 +1,10 @@
 import re
 
 
-def create_url(command, path):
-    return " ".join([command, path])
-
-
 def find_matches(possible_endpoints, uri):
-    for key, value in possible_endpoints.items():
-        if re.fullmatch(key, uri):
-            return value
+    for key in possible_endpoints:
+        if re.fullmatch(key[1], uri):
+            return key
 
 
 def get_unique_id_from_url(path: str) -> str:
